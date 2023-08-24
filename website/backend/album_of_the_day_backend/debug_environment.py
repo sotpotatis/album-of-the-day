707b2279b84eacfae9f7f43c6f14814d77f6ec31
William04A <35110380+William04A@users.noreply.github.com>
2023-08-24 19:39:39 +0200

    More fixes... maybe...

--- website/backend/album_of_the_day_backend/debug_environment.py
+++ website/backend/album_of_the_day_backend/debug_environment.py
@@ -4,9 +4,7 @@ import os
 
 for variable_name, variable_value in os.environ.items():
     if len(variable_name) >= 3:  # Add censorings to rest of variable value
-        variable_value_minified = variable_value[0:2] + "*" * len(variable_value)
+        variable_value_minified = variable_value[0:2] + "*" * (len(variable_value) - 3)
     else:  # If variable is too short to be censored
         variable_value_minified = "***"
-    print(
-        f"{variable_name}:{variable_value_minified} (length {variable_value_minified})"
-    )
+    print(f"{variable_name}:{variable_value_minified} (length {len(variable_value)})")
