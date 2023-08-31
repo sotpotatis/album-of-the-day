747c8c7267def7723b9854cd610b02ed9dfd4bd5
William04A <35110380+William04A@users.noreply.github.com>
2023-08-31 21:14:17 +0200

    More testing to ensure context

--- website/backend/album_of_the_day_backend/load_dotenv_to_bash.py
+++ website/backend/album_of_the_day_backend/load_dotenv_to_bash.py
@@ -1,12 +1,14 @@
 """load_dotenv.py
-Script to load a dotenv file into memory."""
+Script to create a Bash script to load a dotenv file."""
 from argparse import ArgumentParser
 from pathlib import Path
 import dotenv, logging, os
 
 logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 cli = ArgumentParser()
 cli.add_argument("source_file", type=Path, help="The dotenv file to load.")
+cli.add_argument("target_file", type=Path, help="The target file to create.")
 cli.add_argument(
     "--no_print_variables",
     help="Will not print the variables that were loaded.",
@@ -19,12 +21,17 @@ arguments = cli.parse_args()
 dotenv.find_dotenv(
     filename=arguments.source_file, raise_error_if_not_found=True
 )  # Look for file
-dotenv.load_dotenv(dotenv_path=arguments.source_file)  # Load file
 if arguments.print_variables:  # Change debug level if variables should be printed
-    logging.basicConfig(level=logging.INFO)
-    # ...and parse and print them!
+    logging.basicConfig(level=logging.DEBUG)
+with open(arguments.target_file, "w") as target_file_object:
+    # Parse variables
     loaded_values = dotenv.dotenv_values(dotenv_path=arguments.source_file)
     for loaded_value_name, loaded_value in loaded_values.items():
-        logger.info(
+        logger.debug(
             f"Loaded {loaded_value_name} (length {len(loaded_value)}) from environment file."
         )
+        target_file_object.write(f'export {loaded_value_name}="{loaded_value}"\n')
+logger.info(
+    f"""Environment variable file created at {arguments.target_file}. Run it with eg.
+chmod +x {arguments.target_file} && bash {arguments.target_file}"""
+)
