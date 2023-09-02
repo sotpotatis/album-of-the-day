0ffa9b105355b052c90ada095436b478579bb5a9
William04A <35110380+William04A@users.noreply.github.com>
2023-09-02 20:43:50 +0200

    Revert missing script line

--- website/backend/album_of_the_day_backend/prepare-environment-and-expand-files.sh
+++ website/backend/album_of_the_day_backend/prepare-environment-and-expand-files.sh
@@ -1,9 +1,9 @@
 #prepare-environment-and-expand-files.sh
 #Prepares an environment and expands files from it.
 #Generate a script that will load the current environment into Bash
-poetry run python debug_environment.py
-poetry run python load_dotenv_to_bash.py "./backend.env" "./load_env.sh" || exit 1
+poetry run python debug_environment.py || exit 1
+poetry run python load_dotenv_to_bash.py "./backend.env" "./load_env.sh"
 chmod +x "./load_env.sh" && ./load_env.sh #Load the environment
-poetry run python debug_environment.py
-# Test for empty variables (files environment variables are not supposed to be empty)
+poetry run python debug_environment.py #Debug the result
+poetry run python ./album_of_the_day_backend/album_of_the_day/expand_files_from_environment.py || exit 1 #Expand files
 poetry run python debug_environment.py #Debug the result
