6a57aea92209f2ea42424637302535be1cfe3795
William04A <35110380+William04A@users.noreply.github.com>
2023-09-02 20:56:19 +0200

    Test this

--- website/backend/album_of_the_day_backend/prepare-environment-and-expand-files.sh
+++ website/backend/album_of_the_day_backend/prepare-environment-and-expand-files.sh
@@ -3,9 +3,9 @@
 #Generate a script that will load the current environment into Bash
 poetry run python debug_environment.py || exit 1
 poetry run python load_dotenv_to_bash.py "./backend.env" "./load_env.sh"
-chmod +x "./load_env.sh" && ./load_env.sh #Load the environment
+chmod +x "./load_env.sh" #Load the environment
 poetry run python debug_environment.py #Debug the result
 cd album_of_the_day_backend || exit 1
-poetry run python ./album_of_the_day/expand_files_from_environment.py || exit 1 #Expand files
+poetry run  ./load_env.sh && python ./album_of_the_day/expand_files_from_environment.py || exit 1 #Expand files
 cd .. || exit 1
 poetry run python debug_environment.py #Debug the result
