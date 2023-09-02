a891f302ed9b80accce38a153e034f9420a513e0
William04A <35110380+William04A@users.noreply.github.com>
2023-09-02 20:49:26 +0200

    (Hopefully) Fix missing path

--- website/backend/album_of_the_day_backend/prepare-environment-and-expand-files.sh
+++ website/backend/album_of_the_day_backend/prepare-environment-and-expand-files.sh
@@ -4,6 +4,7 @@
 poetry run python debug_environment.py || exit 1
 poetry run python load_dotenv_to_bash.py "./backend.env" "./load_env.sh"
 chmod +x "./load_env.sh" && ./load_env.sh #Load the environment
+cd album_of_the_day_backend || exit 1
 poetry run python debug_environment.py #Debug the result
 poetry run python ./album_of_the_day_backend/album_of_the_day/expand_files_from_environment.py || exit 1 #Expand files
 poetry run python debug_environment.py #Debug the result
